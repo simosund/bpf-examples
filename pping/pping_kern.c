4365d70fe096268235771fca509d6f1c6de64bfb
Simon Sundberg <simon.sundberg@kau.se>
2022-11-21 10:54:15 +0100

    pping: Add option to aggregate RTTs
    Add an option -a or --aggregate to provide an aggregate report of RTT
    samples every X seconds. This is currently mutually exclusive with the
    normal per-RTT sample reports.
    
    The aggregated stats are never reset, and thus contain all RTTs since
    the start of tracing. The next commit will change this to reset the
    stats after every report, so that each report only contain the RTTs
    since the last report.
    
    The RTTs are aggregated and reported per IP-prefix. Right now the user
    space component only sets a single global IP-prefix (::/0) which
    catches all IPs. Later commits will add the option for the user to
    provide custom IP-prefixes.
    
    In this intital implementation for aggregating RTTs, the minimum and
    maximum RTT are tracked and all RTTs are added to a histogram. It uses
    a predetermined number of bins of equal width (by default 1000 bins,
    each 1 ms wide), see RTT_AGG_NR_BINS and RTT_AGG_BIN_WIDTH in
    pping.h. In the future this can be changed to use more sophisticated
    histograms that better capture a wide variety of RTTs.
    
    Signed-off-by: Simon Sundberg <simon.sundberg@kau.se>

--- pping/pping_kern.c
+++ pping/pping_kern.c
@@ -147,6 +147,13 @@ struct {
 	__uint(value_size, sizeof(__u32));
 } events SEC(".maps");
 
+struct {
+	__uint(type, BPF_MAP_TYPE_LPM_TRIE);
+	__type(key, struct lpm_trie_key);
+	__type(value, struct aggregated_rtt_stats);
+	__uint(max_entries, 16384);
+	__uint(map_flags, BPF_F_NO_PREALLOC); // Apparently required for LPM maps
+} agg_map SEC(".maps");
 
 // Help functions
 
@@ -637,6 +644,9 @@ static bool is_rate_limited(__u64 now, __u64 last_ts, __u64 rtt)
 static void send_flow_open_event(void *ctx, struct packet_info *p_info,
 				 struct flow_state *rev_flow)
 {
+	if (!config.push_individual_events)
+		return;
+
 	struct flow_event fe = {
 		.event_type = EVENT_TYPE_FLOW,
 		.flow_event_type = FLOW_EVENT_OPENING,
@@ -660,6 +670,9 @@ static void send_flow_open_event(void *ctx, struct packet_info *p_info,
 static void send_flow_event(void *ctx, struct packet_info *p_info,
 			    bool rev_flow)
 {
+	if (!config.push_individual_events)
+		return;
+
 	struct flow_event fe = {
 		.event_type = EVENT_TYPE_FLOW,
 		.flow_event_type = p_info->event_type,
@@ -704,8 +717,11 @@ static void send_map_full_event(void *ctx, struct packet_info *p_info,
 }
 
 static void send_rtt_event(void *ctx, __u64 rtt, struct flow_state *f_state,
-			  struct packet_info *p_info)
+			   struct packet_info *p_info)
 {
+	if (!config.push_individual_events)
+		return;
+
 	struct rtt_event re = {
 		.event_type = EVENT_TYPE_RTT,
 		.timestamp = p_info->time,
@@ -910,6 +926,7 @@ static bool is_local_address(struct packet_info *p_info, void *ctx)
 	       ret == BPF_FIB_LKUP_RET_FWD_DISABLED;
 }
 
+
 static bool is_new_identifier(struct packet_id *pid, struct flow_state *f_state)
 {
 	if (pid->flow.proto == IPPROTO_TCP)
@@ -921,6 +938,29 @@ static bool is_new_identifier(struct packet_id *pid, struct flow_state *f_state)
 		       pid->identifier - f_state->last_id < 1UL << 31;
 
 	return pid->identifier != f_state->last_id;
+}
+
+static void aggregate_rtt(__u64 rtt, struct in6_addr *ip)
+{
+	if (!config.agg_rtts)
+		return;
+
+	struct aggregated_rtt_stats *rtt_agg;
+	struct lpm_trie_key key = { .prefixlen = 128, .ip = *ip };
+	int bin_idx;
+
+	rtt_agg = bpf_map_lookup_elem(&agg_map, &key);
+	if (!rtt_agg)
+		return;
+
+	if (!rtt_agg->min || rtt < rtt_agg->min)
+		rtt_agg->min = rtt;
+	if (rtt > rtt_agg->max)
+		rtt_agg->max = rtt;
+
+	bin_idx = rtt / RTT_AGG_BIN_WIDTH;
+	bin_idx = bin_idx >= RTT_AGG_NR_BINS ? RTT_AGG_NR_BINS - 1 : bin_idx;
+	rtt_agg->bins[bin_idx]++;
 }
 
 /*
@@ -996,6 +1036,7 @@ static void pping_match_packet(struct flow_state *f_state, void *ctx,
 	f_state->srtt = calculate_srtt(f_state->srtt, rtt);
 
 	send_rtt_event(ctx, rtt, f_state, p_info);
+	aggregate_rtt(rtt, &p_info->pid.flow.saddr.ip);
 }
 
 /*
@@ -1087,6 +1128,9 @@ static bool is_flow_old(struct network_tuple *flow, struct flow_state *f_state,
 static void send_flow_timeout_message(void *ctx, struct network_tuple *flow,
 				      __u64 time)
 {
+	if (!config.push_individual_events)
+		return;
+
 	struct flow_event fe = {
 		.event_type = EVENT_TYPE_FLOW,
 		.flow_event_type = FLOW_EVENT_CLOSING,
