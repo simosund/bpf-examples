4b51350b4a005f84ef073210abf6a6d311f9eea7
Simon Sundberg <simon.sundberg@kau.se>
2025-01-09 19:32:00 +0100

    netstacklat: Add netstacklat tool
    Add the tool netstacklat, which measures latency up to different parts
    in the Linux ingress network stack. Base the initial implementation on
    a bpftrace script from Jesper Dangaard Brouer, which requiers the
    kernel to timestamp ingress packets (i.e. set the tstamp member of the
    skb). Hence, the latency recorded by the tool is the difference
    between the kernel timestamping point and various later points in the
    network stack.
    
    In this initial commit, include the eBPF programs for recording the
    networks stack latency at the start of the kernel functions
    tcp_v4_do_rcv(), tcp_data_queue(), and udp_queue_rcv_one_skb(). Use a
    structure making it easy to extend the tool with additional hook
    points in the future. Make the eBPF programs compatible with
    Cloudflare's ebpf_exporter, and use the map helpers (maps.bpf.h) from
    ebpf_exporter to ensure maps are used in a compatible way.
    
    Open code the histogram maps for different hook points as entirely
    separate maps, instead of encoding the different hook points in the
    key of a separate map as ebpf_exporter often does. This avoids costly
    hashmap lookups, as simple array maps can be used instead of hash
    maps.
    
    Also include a minimal user space loader, which loads and attaches the
    eBPF programs. Later commits will extend this loader to also report
    the recorded latencies stored in the BPF maps.
    
    Signed-off-by: Simon Sundberg <simon.sundberg@kau.se>

---
+++ netstacklat/netstacklat.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef NETSTACKLAT_H
+#define NETSTACKLAT_H
+
+// Histograms support values up to 2^30 ns-> 1s
+#define HIST_MAX_LATENCY_SLOT 30
+/*
+ * MAX_LATENCY_SLOT + 1 bin for hist, +1 "bin" for the "sum key"
+ * (https://github.com/cloudflare/ebpf_exporter?tab=readme-ov-file#sum-keys)
+ * that ebpf_exporter expects for exp2 hists, see the _increment_histogram
+ * macro in maps.bpf.h.
+ */
+#define HIST_NBINS (HIST_MAX_LATENCY_SLOT + 2)
+
+#define NS_PER_S 1000000000
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+#endif
+
+#ifndef max
+#define max(a, b)                   \
+	({                          \
+		typeof(a) _a = (a); \
+		typeof(b) _b = (b); \
+		_a > _b ? _a : _b;  \
+	})
+#endif
+
+enum netstacklat_hook {
+	NETSTACKLAT_HOOK_INVALID = 0,
+	NETSTACKLAT_HOOK_TCP_V4_DO_RCV,
+	NETSTACKLAT_HOOK_TCP_DATA_QUEUE,
+	NETSTACKLAT_HOOK_UDP_QUEUE_RCV_ONE,
+	NETSTACKLAT_N_HOOKS,
+};
+
+#endif
+
