3b2254a4a2bd50f0b6e6035788af40a5acabc4c7
Simon Sundberg <simon.sundberg@kau.se>
2025-01-09 19:32:00 +0100

    Add netstacklat tool
    Add the tool netstacklat, which measures latency up to different parts
    in the Linux ingress network stack. Base the initial implementation on
    a bpftrace script from Jesper Dangaard Brouer, which requiers the
    kernel to timestamp ingress packets (i.e. set the tstamp member of the
    skb). Hence, the latency recorded by the tool is the difference
    between the kernel timestamping point and various later points in the
    network stack.
    
    In this initial commit, include the eBPF programs for recording the
    networks stack latency at the start of the kernel functions
    tcp_v4_do_rcv(), tcp_data_queue(), and udp_queue_rcv_one_skb(). Use a
    structure making it easy to extend the tool with additional hook
    points in the future. Make the eBPF programs compatible with
    Cloudflare's ebpf_exporter, and use the map helpers (maps.bpf.h) from
    ebpf_exporter to ensure maps are used in a compatible way.
    
    Open code the histogram maps for different hook points as entirely
    separate maps, instead of encoding the different hook points in the
    key of a separate map as ebpf_exporter often does. This avoids costly
    hashmap lookups, as simple array maps can be used instead of hash
    maps.
    
    Also include a minimal user space loader, which loads and attaches the
    eBPF programs. Later commits will extend this loader to also report
    the recorded latencies stored in the BPF maps.
    
    Signed-off-by: Simon Sundberg <simon.sundberg@kau.se>

---
+++ netstacklat/netstacklat.bpf.c
@@ -0,0 +1,96 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#include "vmlinux_local.h"
+#include <linux/bpf.h>
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+#include "netstacklat.h"
+#include "maps.bpf.h"
+
+char LICENSE[] SEC("license") = "GPL";
+
+static volatile const u64 TAI_OFFSET = (37UL * NS_PER_S);
+
+/* Helpers in maps.bpf.h require any histogram key to be a struct with a bucket member */
+struct hist_key {
+	u32 bucket;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, HIST_NBINS);
+	__type(key, u32);
+	__type(value, u64);
+} tcp_v4_do_rcv_hist SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, HIST_NBINS);
+	__type(key, u32);
+	__type(value, u64);
+} tcp_data_queue_hist SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, HIST_NBINS);
+	__type(key, u32);
+	__type(value, u64);
+} udp_queue_rcv_hist SEC(".maps");
+
+static void *hook_to_histmap(enum netstacklat_hook hook)
+{
+	switch (hook) {
+	case NETSTACK_HOOK_TCP_V4_DO_RCV:
+		return &tcp_v4_do_rcv_hist;
+	case NETSTACKLAT_HOOK_TCP_DATA_QUEUE:
+		return &tcp_data_queue_hist;
+	case NETSTACKLAT_HOOK_UDP_QUEUE_RCV_ONE:
+		return &udp_queue_rcv_hist;
+	default:
+		return NULL;
+	}
+}
+
+static void record_current_netstacklat(struct sk_buff *skb,
+				       enum netstacklat_hook hook)
+{
+	ktime_t delta_ns, skb_tstamp;
+	struct hist_key key;
+
+	if (!skb)
+		return;
+
+	skb_tstamp = BPF_CORE_READ(skb, tstamp);
+	if (skb_tstamp == 0)
+		return;
+
+	delta_ns = bpf_ktime_get_tai_ns() - TAI_OFFSET - skb_tstamp;
+	if (delta_ns < 0)
+		return;
+
+	increment_exp2_histogram_nosync(hook_to_histmap(hook), key, delta_ns,
+					HIST_MAX_LATENCY_SLOT);
+}
+
+SEC("fentry/tcp_v4_do_rcv")
+int BPF_PROG(netstacklat_tcp_v4_do_rcv, struct sock *sk, struct sk_buff *skb)
+{
+	record_current_netstacklat(skb, NETSTACK_HOOK_TCP_V4_DO_RCV);
+	return 0;
+}
+
+SEC("fentry/tcp_data_queue")
+int BPF_PROG(netstacklat_tcp_data_queue, struct sock *sk, struct sk_buff *skb)
+{
+	record_current_netstacklat(skb, NETSTACKLAT_HOOK_TCP_DATA_QUEUE);
+	return 0;
+}
+
+SEC("fentry/udp_queue_rcv_one_skb")
+int BPF_PROG(netstacklat_udp_queue_rcv, struct sock *sk, struct sk_buff *skb)
+{
+	record_current_netstacklat(skb, NETSTACKLAT_HOOK_UDP_QUEUE_RCV_ONE);
+	return 0;
+}
