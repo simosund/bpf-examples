ff91c7460dd9a769cad545e6b9e9c5e265330a4c
Simon Sundberg <simon.sundberg@kau.se>
2025-01-09 19:32:00 +0100

    netstacklat: Add netstacklat tool
    Add the tool netstacklat, which measures latency up to different parts
    in the Linux ingress network stack. Base the initial implementation on
    a bpftrace script from Jesper Dangaard Brouer, which requiers the
    kernel to timestamp ingress packets (i.e. set the tstamp member of the
    skb). Hence, the latency recorded by the tool is the difference
    between the kernel timestamping point and various later points in the
    network stack.
    
    In this initial commit, include the eBPF programs for recording the
    networks stack latency at the start of the kernel functions
    tcp_v4_do_rcv(), tcp_data_queue(), and udp_queue_rcv_one_skb(). Use a
    structure making it easy to extend the tool with additional hook
    points in the future. Make the eBPF programs compatible with
    Cloudflare's ebpf_exporter.
    
    Open code the histogram maps for different hook points as entirely
    separate maps, instead of encoding the different hook points in the
    key of a separate map as ebpf_exporter often does. This avoids costly
    hashmap lookups, as simple array maps can be used instead of hash
    maps.
    
    Also include a minimal user space loader, which loads and attaches the
    eBPF programs. Later commits will extend this loader to also report
    the recorded latencies stored in the BPF maps.
    
    As the macros from maps.bpf.h in ebpf-exporter have some issues,
    reimplement the needed functionality from those macros as functions
    instead of reusing them directly. In addition to avoiding potential
    issues with unexpected expansions, make the new replacement functions
    also address issue #488 [1] in ebpf-exporter, making the histogram
    format compatible with Prometheus (i.e. right-inclusive).
    
    Instead of placing the functions for storing values in histograms kept
    in BPF maps in our own maps.bpf.h header, place them directly in the
    netstacklat.bpf.c source. This ensures that if moved to the
    ebpf-exporter repository the program will still use these functions
    rather than the ebpf-exporter macros, preventing any unforeseen issues
    due to different scope/expansion of the ebpf-exporter macros and
    missing the fix for the aforementioned #488 issue.
    
    However, keep the signature of the increment_exp2_histogram_nosync()
    function identical to the corresponding ebpf-exporter macro, making it
    possible to replace the function with the ebpf-exporter macros without
    changing the call sites. This makes it easy to switch to using the
    ebpf-exporter macros instead if desired (e.g. because the macros have
    been updated to include some debugging). Notably, this requires
    passing the key struct by value, which is otherwise somewhat
    unnecessary for the trivial key used by netstacklat.
    
    Note that the increment_exp2_histogram_nosync() function here only
    implements the needed functionality by netstacklat. This means that it
    only works for ARRAY maps (it assumes all keys already exist, so does
    not zero-initialize them as needed for HASH maps), and only works for
    a single key type (struct hist_key).
    
    [1] https://github.com/cloudflare/ebpf_exporter/issues/488
    
    Signed-off-by: Simon Sundberg <simon.sundberg@kau.se>

---
+++ netstacklat/netstacklat.bpf.c
@@ -0,0 +1,127 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#include "vmlinux_local.h"
+#include <linux/bpf.h>
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+#include "netstacklat.h"
+#include "bits.bpf.h"
+
+char LICENSE[] SEC("license") = "GPL";
+
+static volatile const u64 TAI_OFFSET = (37UL * NS_PER_S);
+
+/* Helpers in maps.bpf.h require any histogram key to be a struct with a bucket member */
+struct hist_key {
+	u32 bucket;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, HIST_NBINS);
+	__type(key, u32);
+	__type(value, u64);
+} netstack_latency_tcp_v4_do_rcv_seconds SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, HIST_NBINS);
+	__type(key, u32);
+	__type(value, u64);
+} netstack_latency_tcp_data_queue_seconds SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, HIST_NBINS);
+	__type(key, u32);
+	__type(value, u64);
+} netstack_latency_udp_queue_rcv_seconds SEC(".maps");
+
+static u32 get_exp2_histogram_bin_idx(u64 value, u32 max_bin)
+{
+	u32 bin = log2l(value);
+
+	// Right-inclusive histogram, so "round up" the log value
+	if (bin > 0 && 1ULL << bin < value)
+		bin++;
+
+	if (bin > max_bin)
+		bin = max_bin;
+
+	return bin;
+}
+
+static void increment_exp2_histogram_nosync(void *map, struct hist_key key,
+					    u64 value, u32 max_bin)
+{
+	u64 *bin_count;
+
+	// Increment histogram
+	key.bucket = get_exp2_histogram_bin_idx(value, max_bin);
+	bin_count = bpf_map_lookup_elem(map, &key);
+	if (bin_count)
+		(*bin_count)++;
+
+	// Increment sum at end of histogram
+	if (value == 0)
+		return;
+
+	key.bucket = max_bin + 1;
+	bin_count = bpf_map_lookup_elem(map, &key);
+	if (bin_count)
+		*bin_count += value;
+}
+
+static void *hook_to_histmap(enum netstacklat_hook hook)
+{
+	switch (hook) {
+	case NETSTACKLAT_HOOK_TCP_V4_DO_RCV:
+		return &netstack_latency_tcp_v4_do_rcv_seconds;
+	case NETSTACKLAT_HOOK_TCP_DATA_QUEUE:
+		return &netstack_latency_tcp_data_queue_seconds;
+	case NETSTACKLAT_HOOK_UDP_QUEUE_RCV_ONE:
+		return &netstack_latency_udp_queue_rcv_seconds;
+	default:
+		return NULL;
+	}
+}
+
+static void record_latency_since(ktime_t tstamp, enum netstacklat_hook hook)
+{
+	struct hist_key key = { 0 };
+	ktime_t delta, now;
+
+	if (tstamp <= 0)
+		return;
+
+	now = bpf_ktime_get_tai_ns() - TAI_OFFSET;
+	if (tstamp > now)
+		return;
+
+	delta = now - tstamp;
+	increment_exp2_histogram_nosync(hook_to_histmap(hook), key, delta,
+					HIST_MAX_LATENCY_SLOT);
+}
+
+SEC("fentry/tcp_v4_do_rcv")
+int BPF_PROG(netstacklat_tcp_v4_do_rcv, struct sock *sk, struct sk_buff *skb)
+{
+	record_latency_since(skb->tstamp, NETSTACKLAT_HOOK_TCP_V4_DO_RCV);
+	return 0;
+}
+
+SEC("fentry/tcp_data_queue")
+int BPF_PROG(netstacklat_tcp_data_queue, struct sock *sk, struct sk_buff *skb)
+{
+	record_latency_since(skb->tstamp, NETSTACKLAT_HOOK_TCP_DATA_QUEUE);
+	return 0;
+}
+
+SEC("fentry/udp_queue_rcv_one_skb")
+int BPF_PROG(netstacklat_udp_queue_rcv, struct sock *sk, struct sk_buff *skb)
+{
+	record_latency_since(skb->tstamp, NETSTACKLAT_HOOK_UDP_QUEUE_RCV_ONE);
+	return 0;
+}
